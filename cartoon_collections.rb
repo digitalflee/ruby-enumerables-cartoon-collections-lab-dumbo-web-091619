adc0e615c2942f17e238b69d89c96772d190cb4a
Keenan Alves <keenan.alves@gmail.com>
2019-09-06 19:16:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ planeteer_calls.map{|plan| plan.capitalize + "!"}
 end
 
 def long_planeteer_calls(long_planeteer_calls)
-puts kap = long_planeteer_calls.select {|plan| plan.length > 4 } 
+kap = long_planeteer_calls.select {|plan| plan.length > 4 } 
 if kap[0].length > 4 
   true
 end 
